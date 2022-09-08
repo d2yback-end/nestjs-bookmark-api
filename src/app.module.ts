dbbd0353c5b679c66fe52250e19bd039f150f37a
Adi Munawar <munawar.adi31@gmail.com>
2022-09-08 12:41:27 +0700

    create prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [UserModule, AuthModule, BookmarkModule],
+  imports: [UserModule, AuthModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
