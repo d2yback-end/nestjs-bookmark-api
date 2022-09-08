bc723589ae4f5e3ccfebc3f2dfa7a10d0757057f
Adi Munawar <munawar.adi31@gmail.com>
2022-09-08 13:31:39 +0700

    create dto class and implements pipes validator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDTO) {
+    console.log({ dto });
+
     return this.authService.signup();
   }
 
