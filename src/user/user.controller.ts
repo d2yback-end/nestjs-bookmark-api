473c163e3e159c11ec262a1e1d635f56b6c0265f
Adi Munawar <munawar.adi31@gmail.com>
2022-09-11 09:37:33 +0700

    Auth Guard Implementations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-  @Get()
-  getMe() {
-    return 'user info';
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
